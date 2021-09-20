22872f6f70c387bcbb35cb57c5652c28e121b66b
Thejas-1 <naiktheju@gmail.com>
2021-09-20 02:34:33 -0700

    Created Contract class to keep track of Respiratory Data

--- app/src/main/java/com/example/covidbase/RespiratoryData.java
+++ app/src/main/java/com/example/covidbase/RespiratoryData.java
@@ -1,4 +1,14 @@
 package com.example.covidbase;
 
+import android.provider.BaseColumns;
+
 public class RespiratoryData {
+    private RespiratoryData() {};
+
+    public static class Respiratory implements BaseColumns {
+        public static final String TABLE_NAME = "RESPIRATORY_DATA";
+        public static final String INDEX = "INDEX";
+        public static final String ACCEL_READINGS = "VARIATIONS";
+    }
+
 }
