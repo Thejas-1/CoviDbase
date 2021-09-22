217a6f89012201cef066b840a92fc1366f64a675
Thejas-1 <naiktheju@gmail.com>
2021-09-22 03:27:04 -0700

    Added code to record finger tip video for detecting heart rate

--- app/src/main/java/com/example/covidbase/HeartRateService.java
+++ app/src/main/java/com/example/covidbase/HeartRateService.java
@@ -2,12 +2,75 @@ package com.example.covidbase;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.IBinder;
+import android.provider.MediaStore;
+import android.widget.Toast;
 
-public class HeartRateService extends Service {
+import java.io.File;
+
+public class HeartRateService extends Service implements LocationListener {
+    //private static final int VIDEO_CAPTURE = ;
+    /*private Uri fileUri;
+    private static final int VIDEO_CAPTURE = 101;*/
+
     public HeartRateService() {
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getApplicationContext(),"Current Longitute: " + location.getLongitude() + " Current Latitude: " + location.getLatitude(), Toast.LENGTH_LONG).show();
+    }
+
+   /* public void startRecording()
+    {
+        File mediaFile = new
+                File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                + "/myvideo.mp4");
+
+
+
+
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT,5);
+        fileUri = Uri.fromFile(mediaFile);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(intent, VIDEO_CAPTURE);
+    }
+
+    public void onCreate() {
+        File mediaFile = new
+                File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                + "/myvideo.mp4");
+
+        Intent intent =  new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT,5);
+        fileUri = Uri.fromFile(mediaFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        startActivityResult(intent,VIDEO_CAPTURE);
+    }*/
+
+    /*protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent data) {
+
+        if (requestCode == VIDEO_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Video has been saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Failed to record video",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }*/
 
     @Override
     public IBinder onBind(Intent intent) {
--- app/src/main/java/com/example/covidbase/MainActivity.java
+++ app/src/main/java/com/example/covidbase/MainActivity.java
@@ -3,11 +3,78 @@ package com.example.covidbase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private Uri fileUri;
+    private static final int VIDEO_CAPTURE = 101;
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getApplicationContext(),"Current Longitute: " + location.getLongitude() + " Current Latitude: " + location.getLatitude(), Toast.LENGTH_LONG).show();
+    }
+
+    public void startRecording()
+    {
+        /*File mediaFile = new
+                File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                + "/myvideo.mp4");*/
+
+
+
+
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT,5);
+        //fileUri = Uri.fromFile(mediaFile);
+
+        //intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, VIDEO_CAPTURE);
+        }
+
+    }
+
+    //@Override
+    protected void onPostExecute(String text){
+        VideoView vv = (VideoView) findViewById(R.id.videoView);
+        vv.setVideoPath(Environment.getExternalStorageDirectory()+"/my_folder/Action1.mp4");
+        vv.start();
+        Button bt4 = (Button)findViewById(R.id.button3);
+        bt4.setEnabled(true);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == VIDEO_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Video has been saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+                fileUri = data.getData();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Failed to record video",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    //@Override
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +88,10 @@ public class MainActivity extends AppCompatActivity {
         heartRate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent heartRateIntent = new Intent(getApplicationContext(), HeartRateActivity.class);
-                startActivity(heartRateIntent);
+                startRecording();
             }
         });
+
 
         respiratoryRate.setOnClickListener(new View.OnClickListener() {
             @Override
